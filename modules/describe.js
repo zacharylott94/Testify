fc75b64aefd266acab9d2c28e150004187a1dacf
= <zacharylott94@gmail.com>
2019-03-22 15:11:56 -0500

    Code is crazy dirty All current functionality resides in describe.js and wrapper.jas

--- modules/describe.js
+++ modules/describe.js
@@ -2,19 +2,53 @@
 
 export default function describe (description) {
   let object = {
-    queue: [description],
+    description,
+    queue: [],
+    last: [],
     failed: false,
+    log: function() {
+      if (this.last.length === 1) {
+        this.queue.pop()
+      }
+      if (this.failed){
+        console.log(this.description)
+        for (let each of this.queue) {
+          for (let each2 of each) {
+            console.log(each2)
+      
+          }
+        }
+      }
+    },
+    HTML: function(element) {
+      if (this.last.length === 1) {
+        this.queue.pop()
+      }
+      if (this.failed){
+        element.innerHTML += `<h2 class=description>${this.description}</h2>`
+        for (let each of this.queue) {
+          element.innerHTML+= `<h3>${each.shift()}</h3>`
+          for (let each2 of each) {
+            element.innerHTML += `<div class=failure>${each2}</div>`
+      
+          }
+        }
+      }
+    },
     it: function(description) {
-      this.queue.push(description)
+      if (this.last.length === 1) {
+        this.queue.pop()
+      }
+      let functionality = [description]
+      this.queue.push(functionality)
+      this.last = functionality
       return this
     },
     expect: function(val1){
       function toBe(val2) {
         if (val1 !== val2){
-          this.self.queue.push(`Expected ${val2} to be ${val1}`)
+          this.self.last.push(`Expected ${typeof val2}:${val2} to be ${typeof val1}:${val1}`)
           this.self.failed = true
-        } else {
-          this.self.queue.pop()
         }
         return this.self
       }
--- wrapper.js
+++ wrapper.js
@@ -1,14 +1,16 @@
 import describe from './modules/describe.js'
 let window = document.getElementById("main")
 
-let report = describe("test")
+let suite = describe("test")
              .it("should fail")
              .expect(true).toBe(false)
              .expect(1).toBe(2)
              .it("should pass")
              .expect(true).toBe(true)
-if (report.failed){
-  for (let each of report.queue) {
-    console.log(each)
-  }
-}
+             .expect(1).toBe(1)
+             .it("should also fail")
+             .expect('9').toBe(9)
+
+
+suite.log()
+suite.HTML(window)
